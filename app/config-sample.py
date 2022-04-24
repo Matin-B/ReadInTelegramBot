0cf450e9900af467aba718d261aadde03b3e7386
Matin Baloochestani <MatiinBaloochestani@Gmail.com>
2022-04-24 12:44:24 +0430

    Add sample values

--- app/config-sample.py
+++ app/config-sample.py
@@ -1,9 +1,9 @@
-BOT_TOKEN = ""
-BOT_URL = ""
+BOT_TOKEN = "" # Sample: 110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw
+BOT_URL = "" # Sample: https://t.me/IgGramBot?start=ref_web_ReadInTelegramBot
 
 # Database connection
-MONGO_URI = ""
-MONGO_DB = ""
+MONGO_URI = "" # Sample: mongodb://localhost:27017/
+MONGO_DB = "" # Sample: RIT-DB
 
 POCKET_BASE_URL = "https://getpocket.com"
-POCKET_CONSUMER_KEY = ""
+POCKET_CONSUMER_KEY = "" # Sample: 1234-abcd1234abcd1234abcd1234
